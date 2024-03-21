afb5d152d009c551eaf60a8050a0291f3a8c23ef
Pawan Raj <pawansingh201326@gmail.com>
2024-03-21 23:19:36 +0530

    Revert "Department changes"

--- my-app/src/app/pipes/search-department.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchDepartmentPipe } from './search-department.pipe';
-
-describe('SearchDepartmentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchDepartmentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
