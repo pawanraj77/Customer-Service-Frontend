b868bd320f49cd23bb3214525d7421928c4b78d8
jayshree <100516083+jayshree1523@users.noreply.github.com>
2024-03-21 18:24:24 +0530

    Revert merge commit def1796

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,10 +8,24 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule,RouterLink,RouterLinkActive],
+  imports: [RouterOutlet, FormsModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  isAdmin = true;
+
+  constructor() {
+    let user = { name: 'Ford', email: 'ford@gmail.com', role: 'admin' }
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+  checkIsAdmin() {
+    let userStr = localStorage.getItem("user");
+    let useObj = JSON.parse(userStr || '');
+    if (useObj.role === 'admin')
+      return true;
+    else
+      return false;
+  }
+
 }
--- my-app/src/app/components/home/home.component.ts
+++ my-app/src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  cardsData = [
+    { image: 'assets/images/client3.jpg', text: "Prompt and reliable customer service that truly puts the customer first. A pleasure to work with!" },
+    { image: 'assets/images/client2.jpg', text: "The online customer service team exceeded my expectations with their quick responses and helpful guidance. Highly recommended!" },
+    { image: 'assets/images/client.png', text: "I have never encountered such exceptional support before. The team's dedication to solving my issue promptly was truly impressive. Thank you!" }
+  ];
 
+  imageUrl: string = 'assets/images/image.png';
 }
