21fb065dce9d627423caac74bd982fd3459dcb34
mchandu25 <mchandu052524@gmail.com>
2024-03-24 23:11:30 +0530

    updated the changes

--- src/app/model/operator.ts
+++
@@ -1,3 +0,0 @@
-export class Operator {
-    constructor(public operatorId?:number,public firstName?:string,public lastName?:string,public email?:string,public password?:string,public phoneNo?:string,public city?:string,public issuesSolved?:number){}
-}
